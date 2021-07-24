f80a22f0f3cc3f8a5293d1b78b6aa7700c14934d
hyehh <81559324+hyehh@users.noreply.github.com>
2021-07-24 14:53:11 +0900

    2021.07.24 Hyeji First Commit

---
+++ app/src/main/java/com/aosproject/dailyone/decorator/OneDayDecorator.java
@@ -0,0 +1,39 @@
+package com.aosproject.dailyone.decorator;
+
+import android.graphics.Typeface;
+import android.text.style.RelativeSizeSpan;
+import android.text.style.StyleSpan;
+
+import com.prolificinteractive.materialcalendarview.CalendarDay;
+import com.prolificinteractive.materialcalendarview.DayViewDecorator;
+import com.prolificinteractive.materialcalendarview.DayViewFacade;
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+
+import org.threeten.bp.LocalDate;
+
+public class OneDayDecorator implements DayViewDecorator {
+    private CalendarDay date;
+
+    public OneDayDecorator() {
+        date = CalendarDay.today();
+    }
+
+    @Override
+    public boolean shouldDecorate(CalendarDay day) {
+        return date != null && day.equals(date);
+    }
+
+    @Override
+    public void decorate(DayViewFacade view) {
+        // 오늘 날짜.... 꾸미기...
+        view.addSpan(new StyleSpan(Typeface.BOLD));
+        view.addSpan(new RelativeSizeSpan(1.4f));
+    }
+
+    /**
+     * We're changing the internals, so make sure to call {@linkplain MaterialCalendarView#invalidateDecorators()}
+     */
+    public void setDate(LocalDate date) {
+        this.date = CalendarDay.from(date);
+    }
+}
