6eaa8397c0f1a6051e54b85514983e88f9a77ea6
iDoyoung <ido.plum@gmail.com>
2021-07-25 12:54:35 +0900

    Fix bottom navigation listener in Main

--- app/src/main/java/com/aosproject/dailyone/MainActivity.java
+++ app/src/main/java/com/aosproject/dailyone/MainActivity.java
@@ -18,6 +18,7 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 import static com.aosproject.dailyone.R.id.item_fragment1;
 import static com.aosproject.dailyone.R.id.item_fragment2;
 import static com.aosproject.dailyone.R.id.item_fragment3;
+import static com.aosproject.dailyone.R.id.item_fragment4;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,45 +29,60 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         bottomNavigationView = findViewById(R.id.tabar_nav);
+        bottomNavigationView.setSelectedItemId(R.id.item_fragment1);
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                BottomNavigate(item.getItemId());
-                return true;
+                switch (item.getItemId()) {
+                    case item_fragment1:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.content_view_frame, new DiaryFragment()).commit();
+                        return true;
+                    case item_fragment2:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.content_view_frame, new CalendarFragment()).commit();
+                        return true;
+                    case item_fragment3:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.content_view_frame, new ListFragment()).commit();
+                        return true;
+                    case item_fragment4:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.content_view_frame, new SearchFragment()).commit();
+                        return true;
+                }
+                //BottomNavigate(item.getItemId());
+                return false;
             }
         });
-        bottomNavigationView.setSelectedItemId(R.id.item_fragment1);
     }
 
-    private void BottomNavigate(int id) {
-        String tag = String.valueOf(id);
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-
-        Fragment currentFragment = fragmentManager.getPrimaryNavigationFragment();
-        if(currentFragment != null) {
-            fragmentTransaction.hide(currentFragment);
-        }
-        Fragment fragment = fragmentManager.findFragmentByTag(tag);
-        if (fragment == null) {
-            if (id == item_fragment1){
-                fragment = new DiaryFragment();
-            } else if (id==item_fragment2){
-                fragment = new CalendarFragment();
-            } else if (id== item_fragment3){
-                fragment = new ListFragment();
-            } else {
-                fragment = new SearchFragment();
-            }
-            fragmentTransaction.add(R.id.content_view_frame, fragment, tag);
-        } else {
-            fragmentTransaction.show(fragment);
-        }
-
-        fragmentTransaction.setPrimaryNavigationFragment(fragment);
-        fragmentTransaction.setReorderingAllowed(true);
-        fragmentTransaction.commitNow();
-
-    }
+//    private void BottomNavigate(int id) {
+//        String tag = String.valueOf(id);
+//        FragmentManager fragmentManager = getSupportFragmentManager();
+//        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+//
+//        Fragment currentFragment = fragmentManager.getPrimaryNavigationFragment();
+//        if(currentFragment != null) {
+//            fragmentTransaction.hide(currentFragment);
+//        }
+//        Fragment fragment = fragmentManager.findFragmentByTag(tag);
+//        if (fragment == null) {
+//            if (id == item_fragment1){
+//                fragment = new DiaryFragment();
+//            } else if (id==item_fragment2){
+//                fragment = new CalendarFragment();
+//            } else if (id== item_fragment3){
+//                fragment = new ListFragment();
+//            } else {
+//                fragment = new SearchFragment();
+//            }
+//
+//            fragmentTransaction.add(R.id.content_view_frame, fragment, tag);
+//        } else {
+//            fragmentTransaction.show(fragment);
+//        }
+//
+//        fragmentTransaction.setPrimaryNavigationFragment(fragment);
+//        fragmentTransaction.setReorderingAllowed(true);
+//        fragmentTransaction.commitNow();
+//
+//    }
 
 }
