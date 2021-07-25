130abad714fec1185b4a20b944eb70084610542e
Lee-Dohee <ehgml991210@naver.com>
2021-07-25 20:22:59 +0900

    7/25

--- app/src/main/java/com/aosproject/dailyone/fragment/ListFragment.java
+++ app/src/main/java/com/aosproject/dailyone/fragment/ListFragment.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.Toast;
 
@@ -14,11 +16,18 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.aosproject.dailyone.R;
+import com.aosproject.dailyone.bean.Diary;
 import com.aosproject.dailyone.util.DiaryHelper;
+
+import java.util.ArrayList;
 
 public class ListFragment extends Fragment {
 
-    DiaryHelper diary;
+    DiaryHelper helper;
+    ListAdapter adapter;
+    ArrayList<Diary> diaries = new ArrayList<Diary>();
+
+    ListView listView;
 
     Spinner spinnerMonth = null;
     Spinner spinnerYear = null;
@@ -29,7 +38,13 @@ public class ListFragment extends Fragment {
         setHasOptionsMenu(true);
         View view = inflater.inflate(R.layout.fragment_list, container,false);
 
-//        spinnerYear = findViewById(R.id.select_year);
+        helper = new DiaryHelper(getActivity());
+        adapter = new ListAdapter(getContext(), R.layout.inner_list, diaries);
+        listView.setAdapter(adapter);
+
+        listView = view.findViewById(R.id.listView_lv_diaryList);
+        spinnerYear = view.findViewById(R.id.listView_sp_select_year);
+        spinnerMonth = view.findViewById(R.id.listView_sp_select_month);
 
         return view;
     }
@@ -41,28 +56,33 @@ public class ListFragment extends Fragment {
         SQLiteDatabase DB;
 
         try {
-            DB = diary.getReadableDatabase();                  // select 할 수 있는 권한을 준다.
+            DB = helper.getReadableDatabase();                  // select 할 수 있는 권한을 준다.
             String query = "SELECT content, emoji, date FROM diarydata WHERE;";
             Cursor cursor = DB.rawQuery(query, null);   // Cursor = DB 위치를 알려주는 class
 
             StringBuffer stringBuffer = new StringBuffer();         // ArrayList 대신 StringBuffer (ArrayList는 Bean을 만들어야해서 불편)
             while (cursor.moveToNext()) {                           // cursor.moveToNext() = rs.next()
-                String content = cursor.getString(0);
-                int emoji = cursor.getInt(1);
-                String date = cursor.getString(2);
+                int id = cursor.getInt(0);
+                String content = cursor.getString(1);
+                int emoji = cursor.getInt(2);
+                String date = cursor.getString(3);
 
-                stringBuffer.append("content : " + content + ", emoji : " + emoji + ", date : " + date + "\n");
+                Diary diary = new Diary(id, content, emoji, date);
+                diaries.add(diary);
             }
 
-//            tvResult.setText(stringBuffer.toString());
-
             cursor.close();
-            diary.close();
-//            Toast.makeText(MainActivity.this, "Select OK!", Toast.LENGTH_SHORT).show();
+            helper.close();
 
         }catch(Exception e) {
             e.printStackTrace();
-//            Toast.makeText(MainActivity.this, "Select Error!", Toast.LENGTH_SHORT).show();
+        }
+
+        try {
+            adapter = new ListAdapter(getContext(), R.layout.inner_list, diaries);
+            listView.setAdapter(adapter);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
